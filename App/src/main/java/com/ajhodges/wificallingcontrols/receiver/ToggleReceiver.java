f2d7d8953323fbb1eb88c9010ba4722d8b66f855
Adam Hodges <adam@ajhodges.com>
2016-02-18 10:30:44 -0500

    Fix regression from 'launching other tasker plugins' fix

--- App/src/main/java/com/ajhodges/wificallingcontrols/receiver/ToggleReceiver.java
+++ App/src/main/java/com/ajhodges/wificallingcontrols/receiver/ToggleReceiver.java
@@ -4,8 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.ajhodges.wificallingcontrols.Constants;
 import com.ajhodges.wificallingcontrols.NotCompatibleException;
 import com.ajhodges.wificallingcontrols.bundle.BundleScrubber;
 import com.ajhodges.wificallingcontrols.bundle.PluginBundleManager;
@@ -18,12 +20,13 @@ public class ToggleReceiver extends BroadcastReceiver{
     public static final String TOGGLE_WFC_ACTION = "com.ajhodges.wificallingcontrols.TOGGLE_WFC";
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(!com.twofortyfouram.locale.Intent.ACTION_FIRE_SETTING.equals(intent.getAction()) || !TOGGLE_WFC_ACTION.equals(intent.getAction())){
+        if(!(com.twofortyfouram.locale.Intent.ACTION_FIRE_SETTING.equals(intent.getAction()) ||
+                                              TOGGLE_WFC_ACTION.equals(intent.getAction()))){
             //unexpected intent
             return;
         }
-        BundleScrubber.scrub(intent);
 
+        BundleScrubber.scrub(intent);
         final Bundle bundle = intent.getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
         BundleScrubber.scrub(bundle);
 
@@ -35,6 +38,7 @@ public class ToggleReceiver extends BroadcastReceiver{
                 WifiCallingManager wifiCallingManager = WifiCallingManager.getInstance(context);
 
                 if(mode < WifiCallingManager.PREFER_WIFI) {
+                    Log.v(Constants.LOG_TAG, "Toggling WFC");
                     wifiCallingManager.toggleWifi(context, mode);
                     context.getSharedPreferences("ToggleWidgetProvider", Context.MODE_PRIVATE).edit().putBoolean("widgetUpdating", false);
                 }
