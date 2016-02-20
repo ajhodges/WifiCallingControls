c6dbbf2b44e1ad3d83d223901a2dedede2831ad2
Adam Hodges <adam@ajhodges.com>
2016-02-20 18:24:49 -0500

    Improve Samsung support

--- App/src/main/java/com/ajhodges/wificallingcontrols/ipphone/SamsungCallingManager.java
+++ App/src/main/java/com/ajhodges/wificallingcontrols/ipphone/SamsungCallingManager.java
@@ -1,52 +1,18 @@
 package com.ajhodges.wificallingcontrols.ipphone;
-
-import android.content.ContentResolver;
 import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.ajhodges.wificallingcontrols.Constants;
 import com.ajhodges.wificallingcontrols.R;
-
-
-import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import dalvik.system.DexClassLoader;
+import com.samsung.tmowfc.wfcutils.WfcDbHelper;
 
 /**
  * Created by Adam on 4/7/2014.
  */
 public class SamsungCallingManager extends WifiCallingManager{
-    private static Class<?> WfcDbHelper = null;
-    private static Class<? extends Enum> WfcDbHelperRegistration = null;
-    private static Class<? extends Enum> WfcDbHelperProfile = null;
-
-    private static Method getRegister = null;
-    private static Method setRegister = null;
-
-    private static Method getProfile = null;
-    private static Method setProfile = null;
-
     private SamsungCallingManager(Context context){
-        File dexOutputDir = context.getDir("dex", Context.MODE_PRIVATE);
-        DexClassLoader loader = new DexClassLoader(apkFile, dexOutputDir.getAbsolutePath(), null, context.getClassLoader());
-        try {
-            WfcDbHelper = loader.loadClass("com.samsung.tmowfc.wfcutils.WfcDbHelper");
-            WfcDbHelperRegistration = (Class<? extends Enum>)loader.loadClass("com.samsung.tmowfc.wfcutils.WfcDbHelper$RegisterContract$State");
-            WfcDbHelperProfile = (Class<? extends Enum>)loader.loadClass("com.samsung.tmowfc.wfcutils.WfcDbHelper$ProfileContract$Profile");
 
-            getRegister = WfcDbHelper.getMethod("getRegister", new Class[]{ContentResolver.class});
-            setRegister = WfcDbHelper.getMethod("setRegister", new Class[]{ContentResolver.class, WfcDbHelperRegistration});
-
-            getProfile = WfcDbHelper.getMethod("getProfile", new Class[]{ContentResolver.class});
-            setProfile = WfcDbHelper.getMethod("setProfile", new Class[]{ContentResolver.class, WfcDbHelperProfile});
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
     }
 
     private static SamsungCallingManager instance = null;
@@ -61,17 +27,13 @@ public class SamsungCallingManager extends WifiCallingManager{
     public boolean getIPPhoneEnabled(Context context) {
         //Use reflection to get the current state of Wifi Calling
         Boolean ipphoneEnabled = false;
-        try {
-            Enum registerContractState = (Enum)getRegister.invoke(null, context.getContentResolver());
-            ipphoneEnabled = (registerContractState.name().equals("REGISTER"));
-        } catch (InvocationTargetException e) {
-            Toast moveToSystem = Toast.makeText(context.getApplicationContext(), R.string.system_wfc_toast, Toast.LENGTH_SHORT);
-            moveToSystem.show();
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            Log.e(Constants.LOG_TAG, "ERROR: This app is not compatible with your phone");
+        try{
+            WfcDbHelper.RegisterContract.State registerContractState = WfcDbHelper.getRegister(context.getContentResolver());
+            ipphoneEnabled = (registerContractState.equals(WfcDbHelper.RegisterContract.State.REGISTER));
+            Log.d(Constants.LOG_TAG, "Current wifi calling state: " + (ipphoneEnabled ? "on" : "off"));
+        } catch (SecurityException e){
+            Toast.makeText(context.getApplicationContext(), R.string.system_wfc_toast, Toast.LENGTH_SHORT).show();
         }
-        Log.d(Constants.LOG_TAG, "Current wifi calling state: " + (ipphoneEnabled ? "on" : "off"));
         return ipphoneEnabled;
     }
 
@@ -79,11 +41,11 @@ public class SamsungCallingManager extends WifiCallingManager{
     public void toggleWifi(Context context, int mode) {
         boolean ipphoneEnabled = getIPPhoneEnabled(context);
 
-        try {
+        try{
             if (mode == MODE_TOGGLE) {
                 //toggle mode
                 Log.d(Constants.LOG_TAG, "Turning wifi calling " + (!ipphoneEnabled ? "on" : "off") + "...");
-                setRegister.invoke(null, context.getContentResolver(), Enum.valueOf(WfcDbHelperRegistration, (ipphoneEnabled) ? "DONT_REGISTER" : "REGISTER" ));
+                WfcDbHelper.setRegister(context.getContentResolver(), (ipphoneEnabled) ? WfcDbHelper.RegisterContract.State.DONT_REGISTER : WfcDbHelper.RegisterContract.State.REGISTER);
             } else {
                 if ((mode == MODE_ON) == ipphoneEnabled) {
                     //nothing to be done
@@ -92,38 +54,27 @@ public class SamsungCallingManager extends WifiCallingManager{
                 }
                 //set mode enabled/disabled
                 Log.d(Constants.LOG_TAG, "Turning wifi calling " + ((mode == MODE_ON) ? "on" : "off") + "...");
-                setRegister.invoke(null, context.getContentResolver(), Enum.valueOf(WfcDbHelperRegistration, (mode == MODE_ON) ? "REGISTER" : "DONT_REGISTER"));
+                WfcDbHelper.setRegister(context.getContentResolver(), (mode == MODE_ON) ? WfcDbHelper.RegisterContract.State.REGISTER : WfcDbHelper.RegisterContract.State.DONT_REGISTER);
             }
-        } catch(IllegalAccessException e) {
-            Log.e(Constants.LOG_TAG, "ERROR: This app is not compatible with your phone");
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            Toast moveToSystem = Toast.makeText(context.getApplicationContext(), R.string.system_wfc_toast, Toast.LENGTH_SHORT);
-            moveToSystem.show();
-            e.printStackTrace();
+        } catch(SecurityException e){
+            Toast.makeText(context.getApplicationContext(), R.string.system_wfc_toast, Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public int getPreferred(Context context){
         int preferred = -1;
-
-        try {
-            Enum preferredState = (Enum)getProfile.invoke(null, context.getContentResolver());
-            if(preferredState.name().equals("WIFI_PREFERRED")){
+        try{
+            WfcDbHelper.ProfileContract.Profile preferredState = WfcDbHelper.getProfile(context.getContentResolver());
+            if(preferredState.equals(WfcDbHelper.ProfileContract.Profile.WIFI_PREFERRED)){
                 preferred = PREFER_WIFI;
-            } else if(preferredState.name().equals("CELLULAR_NETWORK_PREFERRED")){
+            } else if(preferredState.equals(WfcDbHelper.ProfileContract.Profile.CELLULAR_NETWORK_PREFERRED)){
                 preferred = PREFER_CELL;
-            } else if(preferredState.name().equals("NEVER_USE_CELLULAR_NETWORK")){
+            } else if(preferredState.equals(WfcDbHelper.ProfileContract.Profile.NEVER_USE_CELLULAR_NETWORK)){
                 preferred = PREFER_NEVER_CELL;
             }
-        } catch (IllegalAccessException e) {
-            Log.e(Constants.LOG_TAG, "ERROR: This app is not compatible with your phone");
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            Toast moveToSystem = Toast.makeText(context.getApplicationContext(), R.string.system_wfc_toast, Toast.LENGTH_SHORT);
-            moveToSystem.show();
-            e.printStackTrace();
+        } catch (SecurityException ex){
+            Toast.makeText(context.getApplicationContext(), R.string.system_wfc_toast, Toast.LENGTH_SHORT).show();
         }
         return preferred;
     }
@@ -136,22 +87,17 @@ public class SamsungCallingManager extends WifiCallingManager{
         try {
             switch (preferred) {
                 case PREFER_WIFI:
-                    setProfile.invoke(null, context.getContentResolver(), Enum.valueOf(WfcDbHelperProfile, "WIFI_PREFERRED"));
+                    WfcDbHelper.setProfile(context.getContentResolver(), WfcDbHelper.ProfileContract.Profile.WIFI_PREFERRED);
                     break;
                 case PREFER_CELL:
-                    setProfile.invoke(null, context.getContentResolver(), Enum.valueOf(WfcDbHelperProfile, "CELLULAR_NETWORK_PREFERRED"));
+                    WfcDbHelper.setProfile(context.getContentResolver(), WfcDbHelper.ProfileContract.Profile.CELLULAR_NETWORK_PREFERRED);
                     break;
                 case PREFER_NEVER_CELL:
-                    setProfile.invoke(null, context.getContentResolver(), Enum.valueOf(WfcDbHelperProfile, "NEVER_USE_CELLULAR_NETWORK"));
+                    WfcDbHelper.setProfile(context.getContentResolver(), WfcDbHelper.ProfileContract.Profile.NEVER_USE_CELLULAR_NETWORK);
                     break;
             }
-        } catch (InvocationTargetException e) {
-            Toast moveToSystem = Toast.makeText(context.getApplicationContext(), R.string.system_wfc_toast, Toast.LENGTH_SHORT);
-            moveToSystem.show();
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            Log.e(Constants.LOG_TAG, "ERROR: This app is not compatible with your phone");
-            e.printStackTrace();
+        } catch (SecurityException ex){
+            Toast.makeText(context.getApplicationContext(), R.string.system_wfc_toast, Toast.LENGTH_SHORT).show();
         }
     }
 }
